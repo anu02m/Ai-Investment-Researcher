cc0b11c8d29e69b1bb9953eaecd46c81d3309ac1
anu02m <anuragsharma177029@gmail.com>
2026-07-07 10:19:34 +0530

    connected backend & frontend

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ app.get("/",(req,res) => {
     res.send("Server is running");
 });
 
+app.get("/api/test", (req,res) => {
+    res.json({message: "Hello from the backend"});
+})
+
 app.listen(process.env.PORT,() => {
     console.log(`Server running at port ${process.env.PORT}`);
 });
